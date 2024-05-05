caaccb2b18286f067a4147596e9bc2ac8ec6f3fb
shazidkarim <shazid747@gmail.com>
2024-05-05 11:39:10 +0600

    banner updated

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/Navbar/Navbar";
+import Banner from "../components/Banner/Banner";
 
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,25 @@
-
+import banner_img_1 from "../../../assets/banner_img_1.png";
 
 const Banner = () => {
     return (
-        <div>
-            
+       <>
+       <div className="flex max-w-screen-2xl items-center pt-8 m-auto">
+        <div className="">
+            <h1 className="font-semibold text-start text-[50px] mb-8"> Interior Design. Managed.</h1>
+            <p className="w-[75%] font-sans mb-8 ">Design Manager is all-in-one solution combines project management, accounting, and purchasing, keeping all your project details and finances in one place—and letting you run and grow your business with precision and efficiency.</p>
+            <div className="bg-gray-500 w-[75%] px-8 py-12">
+               <div>
+               <button className="text-white">
+                    start a free trial
+                    </button>
+               </div>
+            </div>
         </div>
+        <div>
+            <img src={banner_img_1} alt="banner_img 1" />
+        </div>
+       </div>
+       </>
     );
 };
 
