c8e2256ea766a9a6d450b6d7ef71f0cd962963e4
shazidkarim <shazid747@gmail.com>
2024-05-05 16:12:07 +0600

    testimonials part added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,6 +3,7 @@ import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
 import Features from "../components/Features/Features";
+import Testimonials from "../components/Testimonials/Testimonials";
 
 
 const Main = () => {
@@ -11,6 +12,7 @@ const Main = () => {
             <Navbar></Navbar>
             <Banner></Banner>
             <Features></Features>
+            <Testimonials></Testimonials>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,11 @@
+
+
+const Testimonials = () => {
+    return (
+        <div>
+            <h1 className="text-black">this is testimonials part </h1>
+        </div>
+    );
+};
+
+export default Testimonials;
