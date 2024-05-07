4cf8e9c203a0df2b49aed91ea9e10615f7d4748c
shazidkarim <shazid747@gmail.com>
2024-05-07 16:37:43 +0600

    icon set up

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import logo from "../../../../assets/design_manager_logo.svg";
-import { IoMdArrowDown } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,12 +17,12 @@ const Navbar = () => {
   return (
     <nav className="bg-[#f9fafb] text-black p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center">
+        <div className="flex items-center ">
           <img src={logo} alt="Design Manager" className="h-8 mr-4" />
           <ul className="hidden md:flex list-none">
             <li className="nav-item">
-              <a href="#" className="text-black hover:text-blue-500 px-3 py-2" onClick={toggleFeaturesMenu}>
-                Features <IoMdArrowDown />
+              <a href="#" className="text-black hover:text-blue-500 px-3 py-2 flex items-center justify-center " onClick={toggleFeaturesMenu}>
+                Features <span> <IoIosArrowDown className="pt-1 text-[20px]" /></span>
               </a>
               {isFeaturesOpen && (
                 <ul className="absolute bg-white shadow-lg rounded-md mt-2">
@@ -41,13 +42,13 @@ const Navbar = () => {
               )}
             </li>
             <li className="nav-item">
-              <a href="#" className="text-black hover:text-blue-500 px-3 py-2">
+              <a href="#" className="text-black hover:text-blue-500 px-3 py-2 flex items-center justify-center ">
                 Pricing
               </a>
           
             </li>
             <li className="nav-item">
-              <a href="#" className="text-black hover:text-blue-500 px-3 py-2">
+              <a href="#" className="text-black hover:text-blue-500 px-3 py-2 flex items-center justify-center ">
                 Resources
               </a>
             </li>
@@ -110,93 +111,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-// import { useState } from "react";
-// import logo from "../../../../assets/design_manager_logo.svg";
-
-// const Navbar = () => {
-//     const [isOpen, setIsOpen] = useState(false);
-
-//     return (
-//         <div>
-//             <nav className="bg-[#f9fafb] text-black p-4">
-//                 <div className="container mx-auto flex justify-between items-center">
-//                     {/* Logo */}
-//                     <div className="flex items-center">
-//                         <img src={logo} alt="Design Manager" className="h-8 ml-4 mr-4" />
-
-//                     </div>
-
-//                     {/* Hamburger Menu (Mobile) */}
-//                     <div className="block lg:hidden">
-//                         <button onClick={() => setIsOpen(!isOpen)}className="text-black focus:outline-none">
-//                             <svg  className="h-6 w-6"  fill="none"  viewBox="0 0 24 24"  stroke="currentColor"  aria-hidden="true">
-//                                 {isOpen ? (
-//                                     <path
-//                                         strokeLinecap="round"
-//                                         strokeLinejoin="round"
-//                                         strokeWidth="2"
-//                                         d="M6 18L18 6M6 6l12 12"
-//                                     />
-//                                 ) : (
-//                                     <path
-//                                         strokeLinecap="round"
-//                                         strokeLinejoin="round"
-//                                         strokeWidth="2"
-//                                         d="M4 6h16M4 12h16m-7 6h7"
-//                                     />
-//                                 )}
-//                             </svg>
-//                         </button>
-//                     </div>
-
-//                     {/* Menu Items (Mobile) */}
-//                     {isOpen && (
-//                         <div className="lg:hidden">
-//                             <ul className="flex flex-col list-none">
-//                                 <li className="nav-item">
-//                                     <a href="#" className="text-black hover:text-blue-500 px-3 py-2">Features</a>
-//                                 </li>
-//                                 <li className="nav-item">
-//                                     <a href="#" className="text-black hover:text-blue-500 px-3 py-2">Pricing</a>
-//                                 </li>
-//                                 <li className="nav-item">
-//                                     <a href="#" className="text-black hover:text-blue-500 px-3 py-2">Resources</a>
-//                                 </li>
-//                                 <li className="nav-item">
-//                                     <a href="#" className="text-black hover:text-blue-500 px-3 py-2">Log In</a>
-//                                 </li>
-//                                 <li className="nav-item">
-//                                     <a href="#" className="bg-[#218554] hover:bg-[#266848] text-white font-mono rounded">Start a Free Trial</a>
-//                                 </li>
-//                             </ul>
-//                         </div>
-//                     )}
-
-//                     {/* Menu Items (Desktop) */}
-//                     <div className="hidden lg:flex">
-//                         <ul className="flex list-none">
-//                             <li className="nav-item">
-//                                 <a href="#" className="text-black hover:text-blue-500 px-3 py-2">Features</a>
-//                             </li>
-//                             <li className="nav-item">
-//                                 <a href="#" className="text-black hover:text-blue-500 px-3 py-2">Pricing</a>
-//                             </li>
-//                             <li className="nav-item">
-//                                 <a href="#" className="text-black hover:text-blue-500 px-3 py-2">Resources</a>
-//                             </li>
-//                             <li className="nav-item">
-//                                 <a href="#" className="text-black hover:text-blue-500 px-8 py-2">Log In</a>
-//                             </li>
-//                             <li className="nav-item">
-//                                 <a href="#" className="bg-[#218554] hover:bg-[#266848] text-white font-semibold px-5 py-3 rounded">Start a Free Trial</a>
-//                             </li>
-//                         </ul>
-//                     </div>
-//                 </div>
-//             </nav>
-//         </div>
-//     );
-// };
-
-// export default Navbar;
